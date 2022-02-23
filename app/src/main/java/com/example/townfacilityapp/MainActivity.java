89ca9175b8699c41401d99b377308dc10f85ea96
Husnain Arshad <Bitm-19-067>
2022-02-23 21:29:17 +0500

    login and Signup Screen For Town Facility app

--- app/src/main/java/com/example/townfacilityapp/MainActivity.java
+++ app/src/main/java/com/example/townfacilityapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.townfacilityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        long delayMilis;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },delayMilis:4000);
+
     }
 }
---
+++ app/src/main/java/com/example/townfacilityapp/New.java
@@ -0,0 +1,4 @@
+package com.example.townfacilityapp;
+
+public class New {
+}
